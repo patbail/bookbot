7a4f28ec76c1862bf377a7e6a89aebe6739751c2
patbail <pbaillairge@gmail.com>
2025-05-15 12:20:27 -0400

    modified the script to the book path as argument

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from stats import *
+import sys
+arg = sys.argv
+filepath = ""
+
+if len(arg) >= 2:
+    filepath = arg[1]
+else:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
     return filepath.read()
 
 def main():
-    with open("books/frankenstein.txt") as f:
+    with open(filepath) as f:
         book_text = get_book_text(f)
         # print(book_text)
         print("============ BOOKBOT ============")
-        print("Analyzing book found at books/frankenstein.txt...")
+        print(f"Analyzing book found at {filepath}...")
         print("----------- Word Count ----------")
         print(f"Found {word_count(book_text)} total words")
         print("--------- Character Count -------")
